806778a358a6c843c14497203431c53542b0cee9
jupottes <juliettepottes@gmail.com>
2019-06-03 22:05:17 -0300

    config firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "<sua-chave>",
-  authDomain: "<sua-chave>",
-  databaseURL: "<sua-chave>",
-  projectId: "<sua-chave>",
-  storageBucket: "<sua-chave>",
-  messagingSenderId: "<sua-chave>",
-  appId: "<sua-chave>"
+  apiKey: "AIzaSyB9mXv9tLPvJsl40Gdm48G_DlTxf-TTrXI",
+  authDomain: "burguer-queen-pottes.firebaseapp.com",
+  databaseURL: "https://burguer-queen-pottes.firebaseio.com",
+  projectId: "burguer-queen-pottes",
+  storageBucket: "burguer-queen-pottes.appspot.com",
+  messagingSenderId: "846388188964",
+  appId: "1:846388188964:web:2a750719880e50a0"
 };
 
 firebase.initializeApp(config);
